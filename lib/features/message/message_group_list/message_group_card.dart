be7a6ebef6eed612c3abdf1b45f24ac45656c7ef
magicpro97 <thlinh.ngo@gmail.com>
2019-11-02 12:40:41 +0700

    Check null for message group card

--- lib/features/message/message_group_list/message_group_card.dart
+++ lib/features/message/message_group_list/message_group_card.dart
@@ -13,73 +13,79 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MessageGroupCard extends StatelessWidget {
-	final MessageGroup messageGroup;
-	final User currentUser;
+  final MessageGroup messageGroup;
+  final User currentUser;
 
-	const MessageGroupCard({
-		Key key,
-		@required this.messageGroup,
-		@required this.currentUser,
-	})  : assert(messageGroup != null),
-			super(key: key);
+  const MessageGroupCard({
+    Key key,
+    @required this.messageGroup,
+    @required this.currentUser,
+  })
+      : assert(messageGroup != null),
+        assert(currentUser != null),
+        super(key: key);
 
-	@override
-	Widget build(BuildContext context) {
-		final screenUtil = ScreenUtil.instance;
+  @override
+  Widget build(BuildContext context) {
+    final screenUtil = ScreenUtil.instance;
 
-		final unReadTextStyle = Theme
-			.of(context)
-			.primaryTextTheme
-			.caption.copyWith(fontSize: screenUtil.setSp(Dimens.h2_size));
-		final readTextStyle = Theme
-			.of(context)
-			.primaryTextTheme
-			.body1.copyWith(fontSize: screenUtil.setSp(Dimens.h2_size));
+    final unReadTextStyle = Theme
+        .of(context)
+        .primaryTextTheme
+        .caption
+        .copyWith(fontSize: screenUtil.setSp(Dimens.h2_size));
+    final readTextStyle = Theme
+        .of(context)
+        .primaryTextTheme
+        .body1
+        .copyWith(fontSize: screenUtil.setSp(Dimens.h2_size));
 
-		return StreamBuilder<Message>(
-			stream: MessageDao.messageListStream(messageGroup.id)
-				.transform(toLatestMessage),
-			builder: (context, snapshot) {
-				if (!snapshot.hasData) {
-					return Container();
-				}
+    return StreamBuilder<Message>(
+        stream: MessageDao.messageListStream(messageGroup.id)
+            .transform(toLatestMessage),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Container();
+          }
 
-				if (snapshot.hasError) {
-					log(snapshot.error);
-				}
+          if (snapshot.hasError) {
+            log(snapshot.error);
+          }
 
-				final latestMessage = snapshot.data;
-				final latestMessageTextStyle =
-				latestMessage.status != MessageStatus.READ &&
-					currentUser.id != latestMessage.userId
-					? unReadTextStyle
-					: readTextStyle;
+          final latestMessage = snapshot.data;
+          final latestMessageTextStyle =
+          latestMessage.status != MessageStatus.READ &&
+              currentUser.id != latestMessage.userId
+              ? unReadTextStyle
+              : readTextStyle;
 
-				return ListTile(
-					leading: CircleAvatar(
-						backgroundImage: CachedNetworkImageProvider(
-							messageGroup.imageUrl,
-						),
-					),
-					title: Row(
-						mainAxisAlignment: MainAxisAlignment.spaceBetween,
-						children: <Widget>[
-							Text(
-								messageGroup.displayName,
-								style: latestMessageTextStyle,
-							),
-							Text(
-								getHourAndMinute(
-									latestMessage.updated ??
-										latestMessage.created,
-								),
-								style: latestMessageTextStyle,
-							),
-						],
-					),
-					subtitle: Text(latestMessage.content, style: latestMessageTextStyle,),
-					onTap: () => goToMessageScreen(context, messageGroup, currentUser),
-				);
-			});
-	}
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: CachedNetworkImageProvider(
+                messageGroup.imageUrl,
+              ),
+            ),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  messageGroup.displayName,
+                  style: latestMessageTextStyle,
+                ),
+                Text(
+                  getHourAndMinute(
+                    latestMessage.updated ?? latestMessage.created,
+                  ),
+                  style: latestMessageTextStyle,
+                ),
+              ],
+            ),
+            subtitle: Text(
+              latestMessage.content,
+              style: latestMessageTextStyle,
+            ),
+            onTap: () => goToMessageScreen(context, messageGroup, currentUser),
+          );
+        });
+  }
 }
