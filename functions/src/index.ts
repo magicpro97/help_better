aea215a9fb3a891a1eff50a828cde601e5770ee9
linhngo <thlinh.ngo@gmail.com>
2019-11-06 10:38:57 +0700

    Remove success logs.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,17 +5,18 @@ admin.initializeApp();
 const db = admin.firestore();
 
 export const createMessageTrigger = functions.firestore
-  .document('message_groups/{messageGroupId}/{messages}/{messageId}')
+  .document("message_groups/{messageGroupId}/{messages}/{messageId}")
   .onWrite((change, context) => {
     if (context.params.messages === "messages") {
       const createTime = change.after.createTime;
       if (change.after.ref.parent.parent !== null) {
         const doc = change.after.ref.parent.parent;
-        return db.collection('message_groups').doc(doc.id)
+        return db
+          .collection("message_groups")
+          .doc(doc.id)
           .update({
             updated: createTime
           })
-          .then(data => console.log(data))
           .catch(err => console.log(err));
       }
     }
