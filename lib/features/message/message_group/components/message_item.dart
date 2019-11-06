0a3f97a6c124cd48ff991d5661ff88ef0c760ff4
linhngo <thlinh.ngo@gmail.com>
2019-11-06 15:29:39 +0700

    Refactor code

--- lib/features/message/message_group/components/message_item.dart
+++ lib/features/message/message_group/components/message_item.dart
@@ -6,95 +6,91 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MessageItem extends StatefulWidget {
-  final Message message;
-  final User currentUser;
-  final User messageOwner;
-  final bool isLastMessage;
-  final bool isFirstMessageGroup;
+    final Message message;
+    final User currentUser;
+    final User messageOwner;
+    final bool isLastMessage;
+    final bool isFirstMessageGroup;
 
-  const MessageItem({
-    Key key,
-    @required this.message,
-    @required this.currentUser,
-    @required this.messageOwner,
-    @required this.isLastMessage,
-    @required this.isFirstMessageGroup,
-  })  : assert(message != null),
-        assert(currentUser != null),
-        assert(messageOwner != null),
-        assert(isLastMessage != null),
-        assert(isFirstMessageGroup != null),
-        super(key: key);
+    const MessageItem({
+        Key key,
+        @required this.message,
+        @required this.currentUser,
+        @required this.messageOwner,
+        @required this.isLastMessage,
+        @required this.isFirstMessageGroup,
+    })  : assert(message != null),
+            assert(currentUser != null),
+            assert(messageOwner != null),
+            assert(isLastMessage != null),
+            assert(isFirstMessageGroup != null),
+            super(key: key);
 
-  @override
-  _MessageItemState createState() => _MessageItemState();
+    @override
+    _MessageItemState createState() => _MessageItemState();
 }
 
 class _MessageItemState extends State<MessageItem> {
-  bool get isFromCurrentUser => widget.currentUser.id == widget.message.userId;
+    bool get isFromCurrentUser => widget.currentUser.id == widget.message.userId;
 
-  bool get showOtherUserAvatar => !isFromCurrentUser && widget.isLastMessage;
+    bool get showOtherUserAvatar => !isFromCurrentUser && widget.isLastMessage;
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: widget.isFirstMessageGroup
-          ? _buildMessage()
-          : _buildMessageNoHeader(),
+    bool get isFirstMessageGroup => widget.isFirstMessageGroup;
+
+    @override
+    Widget build(BuildContext context) {
+        return Container(
+            child:_buildMessage()
+        );
+    }
+
+    Widget _buildMessage() {
+        final screenUtil = ScreenUtil.getInstance();
+
+        return Column(
+            children: <Widget>[
+                isFirstMessageGroup ? Column(
+                    children: <Widget>[
+                        Text(
+                            widget.message.created.toString(),
+                            textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                            height: screenUtil.setHeight(Dimens.normal_space),
+                        ),
+                    ],
+                ) : Container(),
+                ListTile(
+                    leading: showOtherUserAvatar ? _buildOtherUserImage() : null,
+                    title: _buildMessageContent(),
+                ),
+            ],
+        );
+    }
+
+    Widget _buildOtherUserImage() => CircleAvatar(
+        backgroundImage:
+        CachedNetworkImageProvider(widget.messageOwner.photoUrl),
     );
-  }
 
-  Widget _buildMessage() {
-    final screenUtil = ScreenUtil.getInstance();
+    Widget _buildMessageContent() {
+        final screenUtil = ScreenUtil.getInstance();
 
-    return Column(
-      children: <Widget>[
-        Text(
-          widget.message.created.toString(),
-          textAlign: TextAlign.center,
-        ),
-        SizedBox(
-          height: screenUtil.setHeight(Dimens.normal_space),
-        ),
-        ListTile(
-          leading: showOtherUserAvatar ? _buildOtherUserImage() : null,
-          title: _buildMessageContent(),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildMessageNoHeader() {
-    return ListTile(
-      leading: showOtherUserAvatar ? _buildOtherUserImage() : null,
-      title: _buildMessageContent(),
-    );
-  }
-
-  Widget _buildOtherUserImage() =>
-      CircleAvatar(
-        backgroundImage: CachedNetworkImageProvider(
-            widget.messageOwner.photoUrl),
-      );
-
-  Widget _buildMessageContent() {
-    final screenUtil = ScreenUtil.getInstance();
-
-    return Row(
-      mainAxisAlignment:
-      isFromCurrentUser ? MainAxisAlignment.end : MainAxisAlignment.start,
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.all(screenUtil.setHeight(20.0)),
-          decoration: BoxDecoration(
-            color: isFromCurrentUser ? Colors.blue : Colors.grey[400],
-            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-          ),
-          child: Text(
-            widget.message.content,
-          ),
-        ),
-      ],
-    );
-  }
+        return Row(
+            mainAxisAlignment:
+            isFromCurrentUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+            children: <Widget>[
+                Container(
+                    padding: EdgeInsets.all(screenUtil.setHeight(20.0)),
+                    decoration: BoxDecoration(
+                        color: isFromCurrentUser ? Colors.blue : Colors.grey[400],
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    ),
+                    child: Text(
+                        widget.message.content,
+                    ),
+                ),
+            ],
+        );
+    }
 }
--- lib/features/need_help/need_help_tab.dart
+++ lib/features/need_help/need_help_tab.dart
@@ -15,132 +15,132 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NeedHelpTab extends StatefulWidget {
-  final User currentUser;
-  final List<User> friends;
+    final User currentUser;
+    final List<User> friends;
 
-  const NeedHelpTab(
-      {Key key, @required this.currentUser, @required this.friends})
-      : assert(currentUser != null),
-        assert(friends != null),
-        super(key: key);
+    const NeedHelpTab(
+        {Key key, @required this.currentUser, @required this.friends})
+        : assert(currentUser != null),
+            assert(friends != null),
+            super(key: key);
 
-  @override
-  _NeedHelpTabState createState() => _NeedHelpTabState();
+    @override
+    _NeedHelpTabState createState() => _NeedHelpTabState();
 }
 
 class _NeedHelpTabState extends State<NeedHelpTab> {
-  final needHelpBloc = NeedHelpBloc();
+    final needHelpBloc = NeedHelpBloc();
 
-  @override
-  void dispose() {
-    super.dispose();
-    needHelpBloc.close();
-  }
+    @override
+    void dispose() {
+        super.dispose();
+        needHelpBloc.close();
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    final currentUser = widget.currentUser;
+    @override
+    Widget build(BuildContext context) {
+        final currentUser = widget.currentUser;
 
-    return SafeArea(
-      child: StreamBuilder<User>(
-        stream: UserDao.userStream(currentUser.id),
-        builder: (context, snapshot) {
-          if (snapshot.hasError || !snapshot.hasData) {
-            log(snapshot.data.toString());
-            return Center(
-              child: Text("Lỗi"),
-            );
-          }
-          final user = snapshot.data;
-
-          return user.types.contains(UserType.VOLUNTEER)
-              ? _buildHelpList()
-              : _buildInvitation();
-        },
-      ),
-    );
-  }
-
-  Widget _buildInvitation() {
-    final screenUtil = ScreenUtil.instance;
-
-    return Container(
-      child: Column(
-        children: <Widget>[
-          SizedBox(
-            height: screenUtil.setHeight(Dimens.top_space),
-          ),
-          ScreenTitle(
-            title: S.of(context).need_help_title,
-          ),
-          SizedBox(
-            height: screenUtil.setHeight(Dimens.normal_space),
-          ),
-          ScreenCaption(
-            caption: S.of(context).need_help_caption,
-          ),
-          SizedBox(
-            height: screenUtil.setHeight(Dimens.normal_space),
-          ),
-          Image.asset(Assets.help_someone_colors),
-          SizedBox(
-            height: screenUtil.setHeight(Dimens.large_space),
-          ),
-          CupertinoButton(
-            color: Colors.tealAccent[400],
-            child: Text(S.of(context).need_help_action),
-            onPressed: () =>
-                needHelpBloc.add(JoinVolunteerEvent(user: widget.currentUser)),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildHelpList() {
-    final screenUtil = ScreenUtil.instance;
-    final friends = widget.friends;
-
-    return Scaffold(
-        body: Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          SizedBox(
-            height: screenUtil.setHeight(Dimens.top_space),
-          ),
-          ScreenTitle(
-            title: S.of(context).need_help_list_title,
-          ),
-          Expanded(
-            child: StreamBuilder<List<NeedCardItem>>(
-                stream: needHelpBloc.userListStream.transform(
-                    toNeedCardItemList(
-                        context: context,
-                        needHelpBloc: needHelpBloc,
-                        otherUser: friends)),
+        return SafeArea(
+            child: StreamBuilder<User>(
+                stream: UserDao.userStream(currentUser.id),
                 builder: (context, snapshot) {
-                  if (!snapshot.hasData) {
-                    return Container();
-                  }
+                    if (snapshot.hasError || !snapshot.hasData) {
+                        log(snapshot.data.toString());
+                        return Center(
+                            child: Text("Lỗi"),
+                        );
+                    }
+                    final user = snapshot.data;
 
-                  if (snapshot.hasError) {
-                    log(snapshot.data.toString());
-                  }
+                    return user.types.contains(UserType.VOLUNTEER)
+                        ? _buildHelpList()
+                        : _buildInvitation();
+                },
+            ),
+        );
+    }
 
-                  final currentUser = widget.currentUser;
-                  final items = snapshot.data;
-                  items.removeWhere((item) =>
-                      item.user.id == currentUser.id ||
-                      item.user.needs == null);
+    Widget _buildInvitation() {
+        final screenUtil = ScreenUtil.instance;
 
-                  return ListView.builder(
-                      itemCount: items.length,
-                      itemBuilder: (context, index) => items[index]);
-                }),
-          ),
-        ],
-      ),
-    ));
-  }
+        return Container(
+            child: Column(
+                children: <Widget>[
+                    SizedBox(
+                        height: screenUtil.setHeight(Dimens.top_space),
+                    ),
+                    ScreenTitle(
+                        title: S.of(context).need_help_title,
+                    ),
+                    SizedBox(
+                        height: screenUtil.setHeight(Dimens.normal_space),
+                    ),
+                    ScreenCaption(
+                        caption: S.of(context).need_help_caption,
+                    ),
+                    SizedBox(
+                        height: screenUtil.setHeight(Dimens.normal_space),
+                    ),
+                    Image.asset(Assets.help_someone_colors),
+                    SizedBox(
+                        height: screenUtil.setHeight(Dimens.large_space),
+                    ),
+                    CupertinoButton(
+                        color: Colors.tealAccent[400],
+                        child: Text(S.of(context).need_help_action),
+                        onPressed: () =>
+                            needHelpBloc.add(JoinVolunteerEvent(user: widget.currentUser)),
+                    ),
+                ],
+            ),
+        );
+    }
+
+    Widget _buildHelpList() {
+        final screenUtil = ScreenUtil.instance;
+        final friends = widget.friends;
+
+        return Scaffold(
+            body: Container(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                        SizedBox(
+                            height: screenUtil.setHeight(Dimens.top_space),
+                        ),
+                        ScreenTitle(
+                            title: S.of(context).need_help_list_title,
+                        ),
+                        Expanded(
+                            child: StreamBuilder<List<NeedCardItem>>(
+                                stream: needHelpBloc.userListStream.transform(
+                                    toNeedCardItemList(
+                                        context: context,
+                                        needHelpBloc: needHelpBloc,
+                                        otherUser: friends)),
+                                builder: (context, snapshot) {
+                                    if (!snapshot.hasData) {
+                                        return Container();
+                                    }
+
+                                    if (snapshot.hasError) {
+                                        log(snapshot.data.toString());
+                                    }
+
+                                    final currentUser = widget.currentUser;
+                                    final items = snapshot.data;
+                                    items.removeWhere((item) =>
+                                    item.user.id == currentUser.id ||
+                                        item.user.needs == null);
+
+                                    return ListView.builder(
+                                        itemCount: items.length,
+                                        itemBuilder: (context, index) => items[index]);
+                                }),
+                        ),
+                    ],
+                ),
+            ));
+    }
 }
