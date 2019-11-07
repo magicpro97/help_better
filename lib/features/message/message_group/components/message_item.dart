ba7be4255137ab1b086b634619a7ecb0b4ef80ea
linhngo <thlinh.ngo@gmail.com>
2019-11-07 16:05:44 +0700

    Show message status

--- lib/features/message/message_group/components/message_item.dart
+++ lib/features/message/message_group/components/message_item.dart
@@ -6,91 +6,120 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MessageItem extends StatefulWidget {
-    final Message message;
-    final User currentUser;
-    final User messageOwner;
-    final bool isLastMessage;
-    final bool isFirstMessageGroup;
+  final Message message;
+  final User currentUser;
+  final User messageOwner;
+  final bool isLastMessage;
+  final bool isFirstMessageGroup;
 
-    const MessageItem({
-        Key key,
-        @required this.message,
-        @required this.currentUser,
-        @required this.messageOwner,
-        @required this.isLastMessage,
-        @required this.isFirstMessageGroup,
-    })  : assert(message != null),
-            assert(currentUser != null),
-            assert(messageOwner != null),
-            assert(isLastMessage != null),
-            assert(isFirstMessageGroup != null),
-            super(key: key);
+  const MessageItem({
+    Key key,
+    @required this.message,
+    @required this.currentUser,
+    @required this.messageOwner,
+    @required this.isLastMessage,
+    @required this.isFirstMessageGroup,
+  })  : assert(message != null),
+        assert(currentUser != null),
+        assert(messageOwner != null),
+        assert(isLastMessage != null),
+        assert(isFirstMessageGroup != null),
+        super(key: key);
 
-    @override
-    _MessageItemState createState() => _MessageItemState();
+  @override
+  _MessageItemState createState() => _MessageItemState();
 }
 
 class _MessageItemState extends State<MessageItem> {
-    bool get isFromCurrentUser => widget.currentUser.id == widget.message.userId;
+  final screenUtil = ScreenUtil.getInstance();
 
-    bool get showOtherUserAvatar => !isFromCurrentUser && widget.isLastMessage;
+  bool get isFromCurrentUser => widget.currentUser.id == widget.message.userId;
 
-    bool get isFirstMessageGroup => widget.isFirstMessageGroup;
+  bool get showOtherUserAvatar => !isFromCurrentUser && widget.isLastMessage;
 
-    @override
-    Widget build(BuildContext context) {
-        return Container(
-            child:_buildMessage()
-        );
-    }
+  bool get isFirstMessageGroup => widget.isFirstMessageGroup;
 
-    Widget _buildMessage() {
-        final screenUtil = ScreenUtil.getInstance();
+  bool get isSentMessage => widget.message.status == MessageStatus.SENT;
 
-        return Column(
-            children: <Widget>[
-                isFirstMessageGroup ? Column(
-                    children: <Widget>[
-                        Text(
-                            widget.message.created.toString(),
-                            textAlign: TextAlign.center,
-                        ),
-                        SizedBox(
-                            height: screenUtil.setHeight(Dimens.normal_space),
-                        ),
-                    ],
-                ) : Container(),
-                ListTile(
-                    leading: showOtherUserAvatar ? _buildOtherUserImage() : null,
-                    title: _buildMessageContent(),
-                ),
-            ],
-        );
-    }
+  bool get isReadMessage => widget.message.status == MessageStatus.READ;
 
-    Widget _buildOtherUserImage() => CircleAvatar(
-        backgroundImage:
-        CachedNetworkImageProvider(widget.messageOwner.photoUrl),
+  bool get showMessageStatus => isFromCurrentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: _buildMessage());
+  }
+
+  Widget _buildMessage() {
+    return Column(
+      children: <Widget>[
+        isFirstMessageGroup
+            ? Column(
+                children: <Widget>[
+                  Text(
+                    widget.message.created.toString(),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: screenUtil.setHeight(Dimens.normal_space),
+                  ),
+                ],
+              )
+            : Container(),
+        ListTile(
+          leading: showOtherUserAvatar ? _buildOtherUserImage() : null,
+          title: _buildMessageContent(),
+          trailing: showMessageStatus
+              ? isSentMessage ? _buildSentIcon() : _buildUnSendIcon()
+              : null,
+        ),
+      ],
     );
+  }
 
-    Widget _buildMessageContent() {
-        final screenUtil = ScreenUtil.getInstance();
+  Widget _buildSentIcon() => Icon(
+        Icons.check_circle_outline,
+        size: 20,
+        color: Colors.blue,
+      );
 
-        return Row(
-            mainAxisAlignment:
-            isFromCurrentUser ? MainAxisAlignment.end : MainAxisAlignment.start,
-            children: <Widget>[
-                Container(
-                    padding: EdgeInsets.all(screenUtil.setHeight(20.0)),
-                    decoration: BoxDecoration(
-                        color: isFromCurrentUser ? Colors.blue : Colors.grey[400],
-                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    ),
-                    child: Text(
-                        widget.message.content,
-                    ),
-                ),
-            ],
-        );
-    }
+  Widget _buildUnSendIcon() => Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.lens,
+            size: 20.0,
+            color: Colors.blue,
+          ),
+          Icon(
+            Icons.lens,
+            size: 16.0,
+            color: Colors.white,
+          ),
+        ],
+      );
+
+  Widget _buildOtherUserImage() => CircleAvatar(
+        backgroundImage:
+            CachedNetworkImageProvider(widget.messageOwner.photoUrl),
+      );
+
+  Widget _buildMessageContent() {
+    return Row(
+      mainAxisAlignment:
+          isFromCurrentUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(screenUtil.setHeight(20.0)),
+          decoration: BoxDecoration(
+            color: isFromCurrentUser ? Colors.blue : Colors.grey[400],
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          ),
+          child: Text(
+            widget.message.content,
+          ),
+        ),
+      ],
+    );
+  }
 }
