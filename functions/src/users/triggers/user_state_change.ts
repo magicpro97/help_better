a351bda1ee9498d9c272c27870b408e6d8115fd6
magicpro97 <thlinh.ngo@gmail.com>
2019-11-10 16:04:37 +0700

    Change message status when user state change

--- functions/src/users/triggers/user_state_change.ts
+++ functions/src/users/triggers/user_state_change.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions";
-import { DocumentReference } from "@google-cloud/firestore";
-import { FCollection, User, MessageGroup } from "../../constants";
+import { DocumentReference, Timestamp } from "@google-cloud/firestore";
+import { FCollection, User, MessageGroup, Message, MessageStatus } from "../../constants";
 
 export default function onUserStateChange() {
     const { USERS, MESSAGES, MESSAGE_GROUPS } = FCollection;
@@ -8,19 +8,24 @@ export default function onUserStateChange() {
         .document(`${USERS}/{userId}`)
         .onWrite((change, _) => {
             const user = change.after.data() as User;
-            if (user !== undefined && user.online) {
+            if (user && user.online) {
                 const app = (change.after.ref.parent.parent as DocumentReference)
-                return app.collection(`${MESSAGE_GROUPS}`).where("memberIds", "array-contains", user.id).get().then((mGSnap) => {
+                const { SENT, RECEIVED } = MessageStatus;
+                return app.collection(`${MESSAGE_GROUPS}`).where("memberIds", "array-contains", user.id).get().then(async (mGSnap) => {
                     for (const mGDoc of mGSnap.docs) {
                         const messageGroup = mGDoc.data() as MessageGroup;
                         const friendIds = messageGroup.memberIds.filter((id) => id !== user.id);
-                        mGDoc.ref.collection(`${MESSAGES}`).where("status", "==", "SENT").where("userId", "in", friendIds).get().then((mSnap) => {
-                            for (const mDoc of mSnap.docs) {
+                        const mSnap = await mGDoc.ref.collection(`${MESSAGES}`).where("userId", "in", friendIds).get();
+                        for (const mDoc of mSnap.docs) {
+                            const message = mDoc.data() as Message;
+                            if (message.status[user.id] === SENT) {
+                                message.status[user.id] = RECEIVED;
                                 mDoc.ref.update({
-                                    status: "RECEIVED",
-                                }).then().catch(err => console.log(err))
+                                    status: message.status,
+                                    updated: Timestamp.now(),
+                                }).catch(err => console.log(err))
                             }
-                        }).catch(err => console.log(err));
+                        }
                     }
                 }).catch(err => console.log(err));
             }
