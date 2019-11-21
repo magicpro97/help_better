c3225b3ba4edfa9e9fa02e763650055f6ccf2762
magicpro97 <linhnt99x@gmail.com>
2019-11-21 08:07:44 +0700

    Fix import Application

---
+++ android/app/src/main/kotlin/magicpro97/xyz/better_help/Application.kt
@@ -0,0 +1,17 @@
+package magicpro97.xyz.better_help
+
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
+
+class Application : FlutterApplication(), PluginRegistry.PluginRegistrantCallback {
+    override fun registerWith(registry: PluginRegistry) {
+        GeneratedPluginRegistrant.registerWith(registry)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        FlutterFirebaseMessagingService.setPluginRegistrant(this)
+    }
+}
