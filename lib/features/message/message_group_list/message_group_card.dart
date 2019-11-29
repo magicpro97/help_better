8f30a1f970b9835131b67dd05a86ed9c97d74021
magicpro97 <linhnt99x@gmail.com>
2019-11-29 21:53:29 +0700

    Refactor code

--- lib/features/message/message_group_list/message_group_card.dart
+++ lib/features/message/message_group_list/message_group_card.dart
@@ -14,100 +14,93 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MessageGroupCard extends StatelessWidget {
-    final MessageGroup messageGroup;
-    final User currentUser;
-    final List<User> otherUser;
+  final MessageGroup messageGroup;
+  final User currentUser;
+  final List<User> otherUser;
 
-    const MessageGroupCard({
-        Key key,
-        @required this.messageGroup,
-        @required this.currentUser,
-        @required this.otherUser,
-    })
-        : assert(messageGroup != null),
-            assert(currentUser != null),
-            assert(otherUser != null),
-            super(key: key);
+  const MessageGroupCard({
+    Key key,
+    @required this.messageGroup,
+    @required this.currentUser,
+    @required this.otherUser,
+  })  : assert(messageGroup != null),
+        assert(currentUser != null),
+        assert(otherUser != null),
+        super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
-        final screenUtil = ScreenUtil.instance;
-        final unReadTextStyle = Theme
-            .of(context)
-            .primaryTextTheme
-            .caption
-            .copyWith(fontSize: screenUtil.setSp(Dimens.h2_size));
-        final readTextStyle = Theme
-            .of(context)
-            .primaryTextTheme
-            .body1
-            .copyWith(fontSize: screenUtil.setSp(Dimens.h2_size));
+  @override
+  Widget build(BuildContext context) {
+    final screenUtil = ScreenUtil.instance;
+    final unReadTextStyle = Theme.of(context)
+        .primaryTextTheme
+        .caption
+        .copyWith(fontSize: screenUtil.setSp(Dimens.h2_size));
+    final readTextStyle = Theme.of(context)
+        .primaryTextTheme
+        .body1
+        .copyWith(fontSize: screenUtil.setSp(Dimens.h2_size));
 
-        return StreamBuilder<Message>(
-            stream: MessageDao.listStream(messageGroup.id,
+    return StreamBuilder<Message>(
+        stream: MessageDao.listStream(messageGroup.id,
                 orderBy: OrderBy(field: 'created'))
-                .transform(toLatestMessage),
-            builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                    return Container();
-                }
+            .transform(toLatestMessage),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Container();
+          }
 
-                if (snapshot.hasError) {
-                    log(snapshot.error);
-                }
+          if (snapshot.hasError) {
+            log(snapshot.error);
+          }
 
-                final latestMessage = snapshot.data;
-                final latestMessageTextStyle =
-                latestMessage.isSeenMessage() &&
-                    currentUser.id != latestMessage.userId
-                    ? readTextStyle
-                    : unReadTextStyle;
+          final latestMessage = snapshot.data;
+          final latestMessageTextStyle = latestMessage.isSeenMessage() &&
+                  currentUser.id != latestMessage.userId
+              ? readTextStyle
+              : unReadTextStyle;
 
-                return ListTile(
-                    leading: CircleAvatar(
-                        backgroundImage: CachedNetworkImageProvider(
-                            messageGroup.imageUrl ?? otherUser.first.photoUrl,
-                        ),
-                    ),
-                    title: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                            Text(
-                                messageGroup.displayName ??
-                                    displayName(otherUser),
-                                style: latestMessageTextStyle,
-                            ),
-                            Text(
-                                getHourAndMinute(
-                                    latestMessage.updated ??
-                                        latestMessage.created,
-                                ),
-                                style: latestMessageTextStyle.copyWith(
-                                    fontSize: screenUtil.setSp(Dimens.body1_size),
-                                ),
-                            ),
-                        ],
-                    ),
-                    subtitle: Text(
-                        latestMessage.content,
-                        style: latestMessageTextStyle.copyWith(
-                            fontSize: screenUtil.setSp(Dimens.body1_size),
-                        ),
-                    ),
-                    onTap: () =>
-                        goToMessageScreen(
-                            context, messageGroup, currentUser, otherUser),
-                );
-            });
-    }
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: CachedNetworkImageProvider(
+                messageGroup.imageUrl ?? otherUser.first.photoUrl,
+              ),
+            ),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  messageGroup.displayName ?? displayName(otherUser),
+                  style: latestMessageTextStyle,
+                ),
+                Text(
+                  getHourAndMinute(
+                    latestMessage.updated ?? latestMessage.created,
+                  ),
+                  style: latestMessageTextStyle.copyWith(
+                    fontSize: screenUtil.setSp(Dimens.body1_size),
+                  ),
+                ),
+              ],
+            ),
+            subtitle: Text(
+              latestMessage.content,
+              style: latestMessageTextStyle.copyWith(
+                fontSize: screenUtil.setSp(Dimens.body1_size),
+              ),
+            ),
+            onTap: () => goToMessageScreen(
+                context, messageGroup, currentUser, otherUser),
+          );
+        });
+  }
 }
 
 String displayName(List<User> users) {
-    if (users.length > 1) {
-        var name = "";
-        users.forEach((user) => name = name + "${user.displayName}, ");
-        final newName = name.substring(0, name.lastIndexOf(', ')).trim();
-        return newName;
-    }
-    return users.first.displayName;
+  if (users.length > 1) {
+    var name = "";
+    users.forEach((user) => name = name + "${user.displayName}, ");
+    final newName = name.substring(0, name.lastIndexOf(', ')).trim();
+    return newName;
+  }
+  return users.first.displayName;
 }
